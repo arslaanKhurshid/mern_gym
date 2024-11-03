78d947c14004266f81d3771a39de9ad1feae3bfb
arslaanKhurshid <arslaankhurshiedsmt231@gmail.com>
2024-11-03 12:05:54 +0530

    ch

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist' // Ensures the build output goes to "dist"
+    outDir: 'client/dist' // Ensures the build output goes to "dist"
   }
 })
